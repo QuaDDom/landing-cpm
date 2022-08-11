bebe1a4c962f849902ae6830f13cddd4129abd0c
Jadom <matquadev@gmail.com>
2022-08-11 18:40:48 -0300

    [add] first components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "../components/Nav";
+
+export default function Home() {
+  return (
+    <div className="containerAll">
+      <Nav />
+      <div className="content"></div>
+    </div>
+  );
+}
