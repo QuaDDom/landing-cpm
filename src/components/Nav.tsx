d925acba411752e9c3991d9937f85152e8af2638
Jadom <matquadev@gmail.com>
2022-08-11 18:50:24 -0300

    [add] Footer component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Button, Text } from "@nextui-org/react";
 
 export default function Nav() {
-  return <div>Nav</div>;
+  return (
+    <div className="nav">
+      <Text
+        css={{
+          color: "$blue800",
+          fontSize: "$sm",
+          padding: "$2 $4",
+        }}
+      >
+        Using tokens
+      </Text>
+      <Button>Click me</Button>
+    </div>
+  );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Nav from "../components/Nav";
+import { CssBaseline } from "@nextui-org/react";
+import { useTheme, Text } from "@nextui-org/react";
+import Intro from "../components/Intro";
+import About from "../components/About";
 
 export default function Home() {
+  const { theme } = useTheme();
+
   return (
-    <div className="containerAll">
+    <div className="containerAll" style={{}}>
+      <head>{CssBaseline.flush()}</head>
       <Nav />
-      <div className="content"></div>
+      <div className="content">
+        <Intro />
+        <About />
+      </div>
     </div>
   );
 }
