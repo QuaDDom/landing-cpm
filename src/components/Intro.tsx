6194a92b78f9d69760b7620a6621de895cd3a0a4
Jadom <matquadev@gmail.com>
2022-08-12 18:49:57 -0300

    [update] Intro component

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import { Button, Text, Input, Row, Checkbox, Spacer } from "@nextui-org/react";
 
 export default function Intro() {
-  return <div className="intro"></div>;
+  return (
+    <div className="intro">
+      <div className="aboutWe">
+        <Text size={90} h1 weight={"bold"}>
+          Transmitting
+        </Text>
+        <Text
+          size={90}
+          h1
+          weight={"bold"}
+          css={{
+            textGradient: "45deg, $blue600 -50%, $white 100%",
+          }}
+        >
+          Elegance
+        </Text>
+        <Text h2 weight={"normal"} size={30}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, libero.
+        </Text>
+      </div>
+    </div>
+  );
 }
